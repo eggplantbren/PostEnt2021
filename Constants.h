7be408cae27a1aecab16ab9970f4d46669a2c3b3
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-04-25 16:28:33 +1200

    Better constants for demo problem

--- Constants.h
+++ Constants.h
@@ -10,8 +10,8 @@ namespace Constants
 // NS parameters
 static constexpr int num_particles = 100;
 static constexpr int mcmc_steps_per_particle = 1000;
-static constexpr double target_depth_nats = 30.0;
-static constexpr int total_runs = 20;
+static constexpr double target_depth_nats = 20.0;
+static constexpr int total_runs = 1000;
 
 } // namespace Constants
 
