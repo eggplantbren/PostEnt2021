83ac14b815a63656d34af4a32b47ceef68b2c8e9
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-04-25 18:46:25 +1200

    Probably don't need large numbers of particles

--- Constants.h
+++ Constants.h
@@ -8,8 +8,8 @@ namespace Constants
 {
 
 // NS parameters
-static constexpr int num_particles = 100;
-static constexpr int mcmc_steps_per_particle = 1000;
+static constexpr int num_particles = 10;
+static constexpr int mcmc_steps_per_particle = 5000;
 static constexpr double target_depth_nats = 20.0;
 static constexpr int total_runs = 1000;
 
